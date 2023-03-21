ec8c5cc1010a9ea85b225b3e341827168982bfbd
Lingfan Gao <lingfangao@hotmail.com>
2023-03-21 19:11:24 +0100

    initialize

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  const css = ".foo { color: red; }";
+  const { debug, container } = render(
+    <div>
+      Foo <style id="style-children">{css}</style>{" "}
+      <style id="style-dangerous" dangerouslySetInnerHTML={{ __html: css }} />
+    </div>
+  );
+  // style elements will not show up in debug
+  debug();
+  expect(container).toMatchInlineSnapshot(`
+<div>
+  <div>
+    Foo 
+    <style
+      id="style-children"
+    >
+      .foo { color: red; }
+    </style>
+     
+    <style
+      id="style-dangerous"
+    >
+      .foo { color: red; }
+    </style>
+  </div>
+</div>
+`);
 });
